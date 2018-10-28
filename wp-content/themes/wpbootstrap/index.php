d34bc1d77e8f3bdb715ec749424685e84142b998
Попов Данила Александрович <danvop@gmail.com>
2018-10-28 15:40:39 +1000

    include styles and js files

--- wp-content/themes/wpbootstrap/index.php
+++ wp-content/themes/wpbootstrap/index.php
@@ -10,11 +10,11 @@
     <title>Blog Template for Bootstrap</title>
 
     <!-- Bootstrap core CSS -->
-    <link href="css/bootstrap.css" rel="stylesheet">
+    <link href="<?php bloginfo('template_url')?>/css/bootstrap.css" rel="stylesheet">
 
     <!-- Custom styles for this template -->
     <link href="https://fonts.googleapis.com/css?family=Playfair+Display:700,900" rel="stylesheet">
-    <link href="css/blog.css" rel="stylesheet">
+    <link href="<?php bloginfo('stylesheet_url'); ?>" rel="stylesheet">
   </head>
 
   <body>
@@ -231,9 +231,9 @@
     <!-- Placed at the end of the document so the pages load faster -->
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <script>window.jQuery || document.write('<script src="../../assets/js/vendor/jquery-slim.min.js"><\/script>')</script>
-    <script src="js/popper.min.js"></script>
-    <script src="js/bootstrap.js"></script>
-    <script src="js/holder.min.js"></script>
+    <script src="<?php bloginfo('template_url')?>/js/popper.min.js"></script>
+    <script src="<?php bloginfo('template_url')?>/js/bootstrap.js"></script>
+    <script src="<?php bloginfo('template_url')?>/js/holder.min.js"></script>
     <script>
       Holder.addTheme('thumb', {
         bg: '#55595c',
