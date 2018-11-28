033a9f577cb485b41b3acedab83df46b36efa6ba
danvop <danvop@gmail.com>
2018-11-28 22:31:47 +1000

    add loop

--- wp-content/themes/wpbootstrap/functions.php
+++ wp-content/themes/wpbootstrap/functions.php
@@ -7,6 +7,10 @@ require_once get_template_directory() . '/class-wp-bootstrap-navwalker.php';
 // Theme Support
 
 function wpb_theme_setup(){
+    //post thumbnails support
+    //add featured image
+    add_theme_support('post-thumbnails');
+
     //Nav Menus
     register_nav_menus(array(
         'primary' => __('Primary Menu')
@@ -14,3 +18,9 @@ function wpb_theme_setup(){
         
 }
 add_action('after_setup_theme', 'wpb_theme_setup');
+
+function set_excerpt_length(){
+    return 45;
+}
+
+add_filter('excerpt_length', 'set_excerpt_length');
