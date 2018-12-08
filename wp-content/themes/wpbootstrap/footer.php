448273b2592005a1f53d6fabe94df791b7858d1f
Попов Данила Александрович <danvop@gmail.com>
2018-12-08 16:26:54 +1000

    check jquery-slim

--- wp-content/themes/wpbootstrap/footer.php
+++ wp-content/themes/wpbootstrap/footer.php
@@ -21,8 +21,8 @@
     <!-- Bootstrap core JavaScript
     ================================================== -->
     <!-- Placed at the end of the document so the pages load faster -->
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-    <script>window.jQuery || document.write('<script src="../../assets/js/vendor/jquery-slim.min.js"><\/script>')</script>
+    
+    <script>window.jQuery || document.write('<script src="<?php bloginfo('template_url')?>/js/jquery-slim.min.js"><\/script>')</script>
     <script src="<?php bloginfo('template_url')?>/js/popper.min.js"></script>
     <script src="<?php bloginfo('template_url')?>/js/bootstrap.js"></script>
     <script src="<?php bloginfo('template_url')?>/js/holder.min.js"></script>
--- wp-content/themes/wpbootstrap/index.php
+++ wp-content/themes/wpbootstrap/index.php
@@ -44,7 +44,7 @@
       <div class="row">
         <div class="col-md-8 blog-main">
           <h3 class="pb-3 mb-4 font-italic border-bottom">
-            From the Firehose
+            
           </h3>
           <?php if(have_posts()) : ?>
           <?php while(have_posts()) : the_post(); ?>
